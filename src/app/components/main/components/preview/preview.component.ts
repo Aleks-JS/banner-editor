5ebcdc72989417584276003d9c99f35899e76bd0
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-12-04 03:23:59 +0300

    in the component preview.component implemented dynamic resizing of the banner

--- src/app/components/main/components/preview/preview.component.ts
+++ src/app/components/main/components/preview/preview.component.ts
@@ -1,3 +1,5 @@
+import { Subject } from 'rxjs';
+import { ParametersService } from 'src/app/services/parameters.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreviewComponent implements OnInit {
 
-  constructor() { }
+  dynamicStyle = {
+    width: '282px',
+    height: '376px'
+  }
+
+  constructor(private parametersService: ParametersService) { }
 
   ngOnInit(): void {
+    this.parametersService.width$.subscribe(val => this.dynamicStyle.width = `${val}px`)
+    this.parametersService.height$.subscribe(val => this.dynamicStyle.height = `${val}px`)
   }
 
 }
