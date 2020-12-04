f44de49f886b92c57a4b893b4bf6e91f9914f491
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-12-05 02:45:10 +0300

    delete header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
 import { PreviewComponent } from './components/main/components/preview/preview.component';
 import { ParameterFormsComponent } from './components/main/components/parameter-forms/parameter-forms.component';
@@ -14,8 +12,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     MainComponent,
     PreviewComponent,
     ParameterFormsComponent,
