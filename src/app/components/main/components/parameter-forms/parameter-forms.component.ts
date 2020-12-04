424e447f82aa8db79cb29542a57511245f11e5e7
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-12-04 03:22:20 +0300

    in the component parameter-forms, configured methods for interacting with the service

--- src/app/components/main/components/parameter-forms/parameter-forms.component.ts
+++ src/app/components/main/components/parameter-forms/parameter-forms.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { ParametersService } from 'src/app/services/parameters.service';
 
 @Component({
   selector: 'app-parameter-forms',
@@ -19,9 +21,22 @@ export class ParameterFormsComponent implements OnInit {
     url: ['']
   })
 
-  constructor(private fb: FormBuilder) { }
+  width$ = new BehaviorSubject(this.parameterForm.controls['horizontalSize'].value)
+  height$ = new BehaviorSubject(this.parameterForm.controls['verticalSize'].value)
+
+  constructor(private fb: FormBuilder, private parametersService: ParametersService) { }
 
   ngOnInit(): void {
+  }
+
+  onChangeWidth() {
+    this.width$.next(this.parameterForm.controls['horizontalSize'].value)
+    this.width$.subscribe(e => this.parametersService.setWidth(e))
+  }
+
+  onChangeHeight() {
+    this.height$.next(this.parameterForm.controls['verticalSize'].value)
+    this.height$.subscribe(e => this.parametersService.setHeight(e))
   }
 
 }
