2c54d4c2f65b5f1dfb8713a2245eaa8dd84daa98
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-12-05 12:32:08 +0300

    transferring logic into one component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
-import { PreviewComponent } from './components/main/components/preview/preview.component';
-import { ParameterFormsComponent } from './components/main/components/parameter-forms/parameter-forms.component';
-import { ExportBannerComponent } from './components/main/components/export-banner/export-banner.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    PreviewComponent,
-    ParameterFormsComponent,
-    ExportBannerComponent
+    MainComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { ParametersService } from 'src/app/services/parameters.service';
+
+const INIT_WIDTH_PREVIEW: number = 282
+const INIT_HEIGHT_PREVIEW: number = 376
 
 @Component({
   selector: 'app-main',
@@ -7,9 +14,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  parameterForm = this.fb.group({
+    width: [INIT_WIDTH_PREVIEW],
+    height: [INIT_HEIGHT_PREVIEW],
+    bgImage: [null],
+    bgColor: [null],
+    bgGradient: this.fb.group({
+      from: [null],
+      to: [null],
+      direction: [null]
+    }),
+    text: [null],
+    link: [null]
+  })
 
-  ngOnInit(): void {
+  dynamicStyle = {
+    width: `${this.parameterForm.get('width').value}px`,
+    height: `${this.parameterForm.get('height').value}px`
   }
 
+  destroyed$ = new Subject()
+
+  constructor(private fb: FormBuilder, private parametersService: ParametersService) { }
+
+  ngOnInit(): void {
+    this.parameterForm.valueChanges.subscribe(e => {
+      console.log(e)
+    })
+    //   this.parameterForm.valueChanges.pipe(takeUntil(this.destroyed$)).subscribe(e => {
+    //     this.parametersService.setWidth(e.horizontalSize)
+    //     this.parametersService.setHeight(e.verticalSize)
+    // })
+  }
+
+  ngOnDestroy(): void {
+    // this.destroyed$.next()
+    // this.destroyed$.complete()
+  }
+
+
 }
+
+
